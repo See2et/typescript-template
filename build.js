e8012495aea674ecd69d4049c657a1c13f5b62cd
see2et <>
2022-11-05 21:59:19 +0900

    Initial Commit

---
+++ build.js
@@ -0,0 +1,13 @@
+const { resolve } = require("path");
+
+require("esbuild")
+  .build({
+    define: { "process.env.NODE_ENV": process.env.NODE_ENV },
+    entryPoints: [resolve(__dirname, "src/index.ts")],
+    target: "esnext",
+    outfile: resolve(__dirname, "dist/index.js"),
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+  })
+  .catch(() => {});
