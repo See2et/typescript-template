beb9ae218000be791271278bd2be4d48d33edc32
see2et <>
2022-11-06 17:02:37 +0900

    changed build-target version

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ require("esbuild")
   .build({
     define: { "process.env.NODE_ENV": process.env.NODE_ENV },
     entryPoints: [resolve(__dirname, "src/index.ts")],
-    target: "esnext",
+    target: "node18",
     outfile: resolve(__dirname, "dist/index.js"),
     bundle: true,
     minify: true,
